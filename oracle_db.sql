9ab033edeeb9cfc503c1b3f5a48ea7c1247b23d3
jelee68 <40846812+jelee68@users.noreply.github.com>
2018-07-27 14:21:11 +0900

    Add files via upload

---
+++ oracle_db.sql
@@ -0,0 +1,55 @@
+
+CREATE TABLE user_tbl (
+  user_id varchar2(10) NOT NULL PRIMARY KEY,
+  user_pwd varchar2(10) NOT NULL,
+  user_name varchar2(20) NOT NULL,
+  user_birth varchar2(8) NOT NULL,
+  user_addr varchar2(100) NOT NULL,
+  user_tel varchar2(15),
+  adm_check varchar2(1) default '0'
+);
+
+
+CREATE TABLE book_tbl (
+  book_id varchar(10) NOT NULL,
+  book_code char(2) not null,
+  book_name varchar(50) NOT NULL,
+  book_author varchar(30) NOT NULL,
+  book_date char(8) NOT NULL,
+  book_pub  varchar(100) NOT NULL,
+  book_in_date date,
+  book_imgPath varchar(100),
+  PRIMARY KEY (book_id)
+);
+
+CREATE TABLE rent_tbl (
+  rent_no varchar(12) NOT NULL,
+  user_id varchar(10) NOT NULL,
+  book_id varchar(10) not null,
+  book_rent_date date NOT NULL,
+  book_re_due_date date NOT NULL,
+  book_re_date date,
+  book_status char(1) default '1',
+  PRIMARY KEY (rent_no),
+  foreign key(user_id) references user_tbl(user_id),
+  foreign key(book_id) references book_tbl(book_id)
+);
+
+
+create or replace trigger tri3
+after update on user_tbl
+for each row
+begin
+  update rent_tbl
+  set user_id=:new.user_id
+  where user_id=:old.user_id;
+end;
+
+create or replace trigger tri4
+after update on book_tbl
+for each row
+begin
+  update rent_tbl
+  set book_id=:new.book_id
+  where book_id=:old.book_id;
+end;
